007633dd165c718f611c80ea4a5908cba0c787d4
briarwatson <briar.watson402@gmail.com>
2024-01-08 18:45:02 -0600

    new

--- Server.js
+++ Server.js
@@ -4,6 +4,16 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello World"));
 
+app.post("/register", async (req, res) => {
+    try {
+        const hashedPassword = await bcryot.hash(req.body.password, 10);
+        res.send(hashedPassword);
+
+    } catch (err) {
+        res.send(err);
+    }
+})
+
 app.all("*", (req, res) => res.redirect("/"));
 
 app.listen(3000, () => console.log("Server is listening on port 3000"));
